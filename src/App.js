4f95e1de7d797852c7079e6f18115463554d8220
ParkJaeHwi <2606pjh@naver.com>
2024-06-22 17:51:05 +0900

    구조잡기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+
+import Admin from "./components/Admin";
+import Init from "./components/Init";
+import Payment from "./components/Payment";
+import Refund from "./components/Refund";
+import Gift from "./components/Gift";
+import Lotto from "./components/Lotto";
+import Delete from "./components/Delete";
 
 function App() {
+  const [adminFee, setAdminFee] = useState(99999999);
+  const [userPoint, setUserPoint] = useState(852);
+  const [totalLotto, setTotalLotto] = useState(1000000);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main_left">
+        <div className="navigation">
+          <ul>
+            <li>
+              <Link to="/">회원가입</Link>
+            </li>
+            <li>
+              <Link to="/payment">결제</Link>
+            </li>
+            <li>
+              <Link to="/refund">환불</Link>
+            </li>
+            <li>
+              <Link to="/delete">삭제</Link>
+            </li>
+            <li>
+              <Link to="/gift">선물</Link>
+            </li>
+            <li>
+              <Link to="/lotto">Lotto</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Init />} />
+            <Route path="/payment" element={<Payment />} />
+            <Route path="/delete" element={<Delete />} />
+            <Route path="/gift" element={<Gift />} />
+            <Route path="/lotto" element={<Lotto />} />
+            <Route path="/refund" element={<Refund />} />
+          </Routes>
+        </div>
+      </div>
+      <div className="main_right">
+        <Admin adminFee={adminFee} userPoint={userPoint} totalLotto={totalLotto}></Admin>
+      </div>
     </div>
   );
 }
---
+++ src/components/Lotto.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Lotto() {
+  return <div>Lotto</div>;
+}
+
+export default Lotto;
