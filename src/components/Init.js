4b90e74c2d554752516cf97f20490522bec6cd73
Ohhyeon Kwon <zoshs0126@naver.com>
2024-06-26 23:12:55 +0900

    Css수정

--- src/components/Init.js
+++ src/components/Init.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from 'react';
+import '../App.css';
 
-function Init() {
-  return <div>Init</div>;
-}
+const Init = () => {
+  const [ID, setID] = useState('');
+  const [recommend, setRecommend] = useState('');
+
+  return (
+    <div className="init-container">
+      <form onSubmit={''} className="init-form">
+        <div className="form-group">
+          <h2>가입 ID</h2>
+          <input type="text" id="ID" required/>      
+          <h2>추천인 ID</h2>  
+            <input type="text" id="ID" required/>
+            <p></p>
+          <button type="submit" className="submit-button">가입하기</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Init;
